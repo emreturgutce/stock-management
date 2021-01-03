8880d3a420d2129327c30890dc4055a4b5854bf7
emreturgutce <emrebursalee@gmail.com>
2021-01-03 18:49:39 +0300

    change

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,66 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.NODE_ENV) {
-    throw new Error('NODE_ENV must be defined');
-}
-
-if (!process.env.SESSION_SECRET) {
-    throw new Error('NODE_ENV must be defined');
-}
-
-if (!process.env.REDIS_HOST) {
-    throw new Error('REDIS_HOST must be defined');
-}
-
-if (!process.env.REDIS_PORT) {
-    throw new Error('REDIS_PORT must be defined');
-}
-
-if (!process.env.JWT_SECRET) {
-    throw new Error('REDIS_PORT must be defined');
-}
-
-if (!process.env.PORT) {
-    throw new Error('PORT must be defined');
-}
-
-if (!process.env.POSTGRES_USER) {
-    throw new Error('POSTGRES_USER must be defined');
-}
-
-if (!process.env.POSTGRES_PASSWORD) {
-    throw new Error('POSTGRES_PASSWORD must be defined');
-}
-
-if (!process.env.POSTGRES_HOST) {
-    throw new Error('POSTGRES_HOST must be defined');
-}
-
-if (!process.env.POSTGRES_PORT) {
-    throw new Error('POSTGRES_PORT must be defined');
-}
-
-if (!process.env.POSTGRES_DB) {
-    throw new Error('POSTGRES_DB must be defined');
-}
-
-if (!process.env.AWS_ACCESS_KEY_ID) {
-    throw new Error('AWS_ACCESS_KEY_ID must be defined');
-}
-
-if (!process.env.AWS_SECRET_ACCESS_KEY) {
-    throw new Error('AWS_SECRET_ACCESS_KEY must be defined');
-}
-
-if (!process.env.AWS_S3_BUCKET) {
-    throw new Error('AWS_S3_BUCKET must be defined');
-}
-
-if (!process.env.REDIS_PASSWORD) {
-    throw new Error('REDIS_PASSWORD must be defined');
-}
-
 export const {
     NODE_ENV,
     SESSION_SECRET,
