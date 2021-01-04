0221f62911b5cc39ea38c0e4face3b96a2695192
emreturgutce <emrebursalee@gmail.com>
2021-01-04 16:00:00 +0300

    session work

--- src/controller/personel.ts
+++ src/controller/personel.ts
@@ -45,8 +45,6 @@ router.get('/logout', auth, async (req, res, next) => {
             );
     });
 
-    req.session.userId = undefined;
-
     res.clearCookie(COOKIE_NAME);
 
     res.status(204).send();
