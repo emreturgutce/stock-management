92a75915a766fbd66d1082316af8a79a3fe8cba3
emreturgutce <emrebursalee@gmail.com>
2021-01-17 18:32:35 +0300

    supplier tests

---
+++ src/controllers/__tests__/supplier/get.test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import { app } from '../../../app';
+import { loginUser, user } from '../../../utils';
+
+describe('/api/suppliers tests', () => {
+    it('Should return 200 OK for valid cookie', async () => {
+        const response = await request(app)
+            .get('/api/suppliers')
+            .set('Cookie', await loginUser(user))
+            .send()
+            .expect(200);
+
+        expect(response).not.to.be.empty;
+    });
+
+    it.only('Should return 401 Unauthorized for invalid cookie', async () => {
+        const response = await request(app)
+            .get('/api/suppliers')
+            .set('Cookie', 'invalidcookie')
+            .send()
+            .expect(401);
+
+        expect(response).not.to.be.empty;
+    });
+});
