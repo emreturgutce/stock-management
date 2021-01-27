50e2c89f311996656cc9b84f5da761bf0292f0cd
emreturgutce <emrebursalee@gmail.com>
2021-01-27 18:32:27 +0300

    redis url parsing

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,26 +3,37 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 if (process.env.NODE_ENV === 'test') {
-    process.env.POSTGRES_DB = 'stock_management_test';
+	process.env.POSTGRES_DB = 'stock_management_test';
+}
+
+if (process.env.NODE_ENV === 'production') {
+	const tag = process.env.REDIS_URL?.match(/(.*):\/\/(.*?):(.*)@(.*):(.*)/);
+
+	if (tag) {
+		process.env.REDIS_USER = tag[2];
+		process.env.REDIS_PASSWORD = tag[3];
+		process.env.REDIS_HOST = tag[4];
+		process.env.REDIS_PORT = tag[5];
+	}
 }
 
 export const {
-    NODE_ENV,
-    SESSION_SECRET,
-    REDIS_HOST,
-    REDIS_PORT,
-    JWT_SECRET,
-    PORT,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-    POSTGRES_HOST,
-    POSTGRES_PORT,
-    POSTGRES_DB,
-    AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY,
-    AWS_S3_BUCKET,
-    REDIS_PASSWORD,
-    FRONTEND_URL,
+	NODE_ENV,
+	SESSION_SECRET,
+	REDIS_HOST,
+	REDIS_PORT,
+	JWT_SECRET,
+	PORT,
+	POSTGRES_USER,
+	POSTGRES_PASSWORD,
+	POSTGRES_HOST,
+	POSTGRES_PORT,
+	POSTGRES_DB,
+	AWS_ACCESS_KEY_ID,
+	AWS_SECRET_ACCESS_KEY,
+	AWS_S3_BUCKET,
+	REDIS_PASSWORD,
+	FRONTEND_URL,
 } = process.env;
 export * from './redis';
 export * from './database';
