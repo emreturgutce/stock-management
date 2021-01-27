21d683a6d54c2b1c8de889b35d3e6754cf2d2c04
emreturgutce <emrebursalee@gmail.com>
2021-01-27 18:12:22 +0300

    redis connect timeout config added

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,27 +2,28 @@ import Redis, { Redis as RedisType } from 'ioredis';
 import { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } from '.';
 
 class RedisClient {
-    private static client: RedisType;
+	private static client: RedisType;
 
-    static getInstance(): RedisType {
-        if (!RedisClient.client) {
-            RedisClient.client = new Redis({
-                host: REDIS_HOST,
-                password: REDIS_PASSWORD,
-                port: parseInt(REDIS_PORT, 10),
-            });
+	static getInstance(): RedisType {
+		if (!RedisClient.client) {
+			RedisClient.client = new Redis({
+				host: REDIS_HOST,
+				password: REDIS_PASSWORD,
+				port: parseInt(REDIS_PORT, 10),
+				connectTimeout: 10000,
+			});
 
-            RedisClient.client.once('error', (err) => {
-                console.log(`Error occurred connecting Redis: ${err}`.red);
-            });
+			RedisClient.client.once('error', (err) => {
+				console.log(`Error occurred connecting Redis: ${err}`.red);
+			});
 
-            RedisClient.client.once('connect', () => {
-                console.log(`😈 Connected to Redis`.yellow);
-            });
-        }
+			RedisClient.client.once('connect', () => {
+				console.log(`😈 Connected to Redis`.yellow);
+			});
+		}
 
-        return RedisClient.client;
-    }
+		return RedisClient.client;
+	}
 }
 
 export { RedisClient };
