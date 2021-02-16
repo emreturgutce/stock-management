1e626c53f5b3e64cf01c7c16243379f2cee8e5db
Emre Turgut <emrebursalee@gmail.com>
2021-02-16 13:47:09 +0300

    context added to express-session

--- src/typings/custom.d.ts
+++ src/typings/custom.d.ts
@@ -1,39 +1,43 @@
 declare module 'express-session' {
-    interface Session {
-        userId: string;
-    }
+	interface Session {
+		context: {
+			id: string;
+			email: string;
+            verified: boolean;
+		};
+	}
 }
 
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            NODE_ENV: string;
-            SESSION_SECRET: string;
-            REDIS_HOST: string;
-            REDIS_PORT: string;
-            REDIS_PASSWORD: string | undefined;
-            JWT_SECRET: string;
-            PORT: string;
-            POSTGRES_USER: string;
-            POSTGRES_PASSWORD: string;
-            POSTGRES_HOST: string;
-            POSTGRES_PORT: string;
-            POSTGRES_DB: string;
-            AWS_ACCESS_KEY_ID: string;
-            AWS_SECRET_ACCESS_KEY: string;
-            AWS_S3_BUCKET: string;
-        }
-    }
-    interface CookieOptions {
-        sameSite: boolean | 'none' | 'strict' | 'lax' | undefined;
-        secure: boolean;
-        httpOnly: boolean;
-    }
+	namespace NodeJS {
+		interface ProcessEnv {
+			NODE_ENV: string;
+			SESSION_SECRET: string;
+			REDIS_HOST: string;
+			REDIS_PORT: string;
+			REDIS_PASSWORD: string | undefined;
+			JWT_SECRET: string;
+			PORT: string;
+			POSTGRES_USER: string;
+			POSTGRES_PASSWORD: string;
+			POSTGRES_HOST: string;
+			POSTGRES_PORT: string;
+			POSTGRES_DB: string;
+			AWS_ACCESS_KEY_ID: string;
+			AWS_SECRET_ACCESS_KEY: string;
+			AWS_S3_BUCKET: string;
+		}
+	}
+	interface CookieOptions {
+		sameSite: boolean | 'none' | 'strict' | 'lax' | undefined;
+		secure: boolean;
+		httpOnly: boolean;
+	}
 
-    type User = {
-        email: string;
-        password: string;
-    };
+	type User = {
+		email: string;
+		password: string;
+	};
 }
 
 export {};
