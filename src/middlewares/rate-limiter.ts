d8ce180d32e0b1b0035d54e6ec55472e7b9d87d9
Emre Turgut <emrebursalee@gmail.com>
2021-02-17 13:52:50 +0300

    quick change in rate limit middleware

--- src/middlewares/rate-limiter.ts
+++ src/middlewares/rate-limiter.ts
@@ -4,21 +4,21 @@ import { RedisClient } from '../config';
 import { RATE_LIMIT, RATE_LIMIT_PREFIX, RATE_LIMIT_TIME } from '../constants';
 
 const rateLimiterRedis = new RateLimiterRedis({
-    storeClient: RedisClient.getInstance(),
-    keyPrefix: RATE_LIMIT_PREFIX,
-    points: RATE_LIMIT,
-    duration: RATE_LIMIT_TIME,
+	storeClient: RedisClient.getInstance(),
+	keyPrefix: RATE_LIMIT_PREFIX,
+	points: RATE_LIMIT,
+	duration: RATE_LIMIT_TIME,
 });
 
-export const rateLimiter = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
+export const rateLimiter = () => async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
 ) => {
-    try {
-        await rateLimiterRedis.consume(req.ip);
-        next();
-    } catch (err) {
-        res.status(429).json({ message: 'Too many requests', status: 429 });
-    }
+	try {
+		await rateLimiterRedis.consume(req.ip);
+		next();
+	} catch (err) {
+		res.status(429).json({ message: 'Too many requests', status: 429 });
+	}
 };
