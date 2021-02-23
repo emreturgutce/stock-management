ee5fdc1c76ac758057a5ac42b64a18f4b19286c7
emreturgutce <emrebursalee@gmail.com>
2021-02-23 15:48:26 +0300

    resizing and converting to webp

--- src/utils/upload-avatar-to-s3.ts
+++ src/utils/upload-avatar-to-s3.ts
@@ -1,11 +1,18 @@
+import sharp from 'sharp';
 import { AWS_S3_BUCKET, S3Client } from '../config';
 
-export const uploadAvatarToS3 = (avatarId: string, readStream: Buffer) => {
-    return S3Client.getInstance()
-        .upload({
-            Bucket: AWS_S3_BUCKET!,
-            Key: avatarId,
-            Body: readStream,
-        })
-        .promise();
+export const uploadAvatarToS3 = async (
+	avatarId: string,
+	readStream: Buffer,
+) => {
+	return S3Client.getInstance()
+		.upload({
+			Bucket: AWS_S3_BUCKET!,
+			Key: avatarId,
+			Body: await sharp(readStream)
+				.webp({ lossless: true })
+				.resize(680, 480, { fit: 'contain', background: '#fdfdfd' })
+				.toBuffer(),
+		})
+		.promise();
 };
