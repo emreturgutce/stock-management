95b603b4b53767d788d65d6cb9a9de91728749ec
emreturgutce <emrebursalee@gmail.com>
2021-02-27 12:55:35 +0300

    quick change

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './upload-avatar-to-s3';
+export * from './delete-avatar-from-s3';
 export * from './create-confirmation-email-content';
 export * from './create-confirmation-url';
 export * from './create-forgot-password-email-content';
