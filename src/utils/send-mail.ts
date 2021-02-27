79b63bcdbda0520bea0314936c11cd6fa2149795
emreturgutce <emrebursalee@gmail.com>
2021-02-27 19:22:04 +0300

    sengrid removed

--- src/utils/send-mail.ts
+++ src/utils/send-mail.ts
@@ -1,36 +1,26 @@
 import nodemailer, { Transporter } from 'nodemailer';
-import { NODE_ENV, SENDGRID_PASSWORD, SENDGRID_USERNAME } from '../config';
+import { NODE_ENV } from '../config';
 
 let transporter: Transporter;
 
-if (NODE_ENV !== 'production') {
-	nodemailer.createTestAccount((error, testAccount) => {
-		if (error) {
-			console.error(`Could not create mail test account: ${error}`);
-			process.exit(1);
-		}
+nodemailer.createTestAccount((error, testAccount) => {
+	if (error) {
+		console.error(`Could not create mail test account: ${error}`);
+		process.exit(1);
+	}
 
-		const transportOptions = {
-			host: 'smtp.ethereal.email',
-			port: 587,
-			secure: false,
-			auth: {
-				user: testAccount.user,
-				pass: testAccount.pass,
-			},
-		};
-
-		transporter = nodemailer.createTransport(transportOptions);
-	});
-} else {
-	transporter = nodemailer.createTransport({
-		service: 'SendGrid',
+	const transportOptions = {
+		host: 'smtp.ethereal.email',
+		port: 587,
+		secure: false,
 		auth: {
-			user: SENDGRID_USERNAME,
-			password: SENDGRID_PASSWORD,
+			user: testAccount.user,
+			pass: testAccount.pass,
 		},
-	} as any);
-}
+	};
+
+	transporter = nodemailer.createTransport(transportOptions);
+});
 
 export async function sendEmail(mailOptions: nodemailer.SendMailOptions) {
 	const info = await transporter.sendMail(mailOptions);
