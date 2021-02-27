b0d438c4ac4bddf12fa514ca578477fcd0d6fd09
emreturgutce <emrebursalee@gmail.com>
2021-02-27 16:21:29 +0300

    cors fix

--- src/config/cors-options.ts
+++ src/config/cors-options.ts
@@ -1,6 +1,9 @@
-import { FRONTEND_URL } from '.';
+import { FRONTEND_URL, AWS_S3_BUCKET } from '.';
 
 export const corsOptions = {
-    origin: FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
+	origin: [
+		FRONTEND_URL || 'http://localhost:3000',
+		`https://${AWS_S3_BUCKET}.s3-eu-west-1.amazonaws.com`,
+	],
+	credentials: true,
 };
