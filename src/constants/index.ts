6f133ff43d7d514b0266b3369b439319f325288f
emreturgutce <emrebursalee@gmail.com>
2021-03-07 15:01:55 +0300

    rate-limiter req amount reduced

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const COOKIE_EXPIRATION = 1000 * 60 * 60 * 24 * 7 * 365;
 export const COOKIE_NAME = 'auth_token';
 export const RATE_LIMIT_TIME = 60;
-export const RATE_LIMIT = 25;
+export const RATE_LIMIT = 15;
 export const RATE_LIMIT_PREFIX = 'ratelimit';
 export const CONFIRM_USER_PREFIX = 'user-confirmation:';
 export const SESSION_PREFIX = `stock-management-session:`;
