438bb0fc4da5ea619f7ba926e50d29cffbb2eda0
emreturgutce <emrebursalee@gmail.com>
2021-03-14 14:25:59 +0300

    postgres ssl fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,40 +1,43 @@
 import { Pool } from 'pg';
 import {
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-    POSTGRES_HOST,
-    POSTGRES_PORT,
-    POSTGRES_DB,
-    NODE_ENV,
+	POSTGRES_USER,
+	POSTGRES_PASSWORD,
+	POSTGRES_HOST,
+	POSTGRES_PORT,
+	POSTGRES_DB,
+	NODE_ENV,
 } from '.';
 
 class DatabaseClient {
-    private static pool: Pool;
+	private static pool: Pool;
 
-    static getInstance(): Pool {
-        if (!DatabaseClient.pool) {
-            DatabaseClient.pool = new Pool({
-                user: POSTGRES_USER,
-                host: POSTGRES_HOST,
-                database: POSTGRES_DB,
-                password: POSTGRES_PASSWORD,
-                port: parseInt(POSTGRES_PORT, 10),
-                ssl: NODE_ENV === 'production',
-            });
+	static getInstance(): Pool {
+		if (!DatabaseClient.pool) {
+			DatabaseClient.pool = new Pool({
+				user: POSTGRES_USER,
+				host: POSTGRES_HOST,
+				database: POSTGRES_DB,
+				password: POSTGRES_PASSWORD,
+				port: parseInt(POSTGRES_PORT, 10),
+				ssl:
+					NODE_ENV === 'production'
+						? { rejectUnauthorized: false }
+						: false,
+			});
 
-            DatabaseClient.pool.once('connect', () => {
-                console.log(`🐘 Connected to db`.blue);
-            });
+			DatabaseClient.pool.once('connect', () => {
+				console.log(`🐘 Connected to db`.blue);
+			});
 
-            DatabaseClient.pool.once('error', (err) => {
-                console.log(`Error occurred connecting db: ${err}`.red);
-            });
+			DatabaseClient.pool.once('error', (err) => {
+				console.log(`Error occurred connecting db: ${err}`.red);
+			});
 
-            DatabaseClient.pool.query('SELECT 1');
-        }
+			DatabaseClient.pool.query('SELECT 1');
+		}
 
-        return DatabaseClient.pool;
-    }
+		return DatabaseClient.pool;
+	}
 }
 
 DatabaseClient.getInstance();
